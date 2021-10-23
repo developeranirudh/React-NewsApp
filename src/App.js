53783f268daba3dc2b136f23fdc1ae6ed9f698ae
Anirudh <developeranirudhgupta@gmail.com>
2021-10-24 00:31:43 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import React, { Component } from 'react'
 import News from './components/News';
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar/>
-        <News/>
-      </div>
-    )
-  }
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
+export default function App() {
+ 
+  return (
+  <div>
+     <Router>
+     <Navbar/>
+         <Switch> 
+           <Route exact path="/">
+           <News key="general" category="general"></News>
+          </Route>
+          <Route exact path="/business">
+          <News key="business" category="business"></News>
+          </Route>
+
+          <Route exact path="/entertainment">
+          <News  key="entertainment" category="entertainment"></News>
+          </Route>
+
+          <Route exact path="/sport">
+          <News  key="sport"category="sport"></News>
+          </Route>
+
+          <Route  exact path="/technology">
+          <News key="technology" category="technology"></News>
+          </Route>
+
+        </Switch>
+</Router>
+</div>
+  );
 }
+ 
+
 
  
--- src/components/News.js
+++ src/components/News.js
@@ -1,38 +1,51 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React,{useEffect} from 'react';
 import { useState } from 'react';
 import Newsitem from './Newsitem';
 // import data from './sampledata.json';
 import './News.css';
 import Sipnner from './Sipnner';
-export default function News() {
-const [btndisable,Setbtndisable]=useState(0);
- const [newsdata,Setnewsdata]=useState([]);
+export default function News(props) {
+const[load,setload]=useState(true);
+const [page,Setpage]=useState(1);
+const [newsdata,Setnewsdata]=useState([]);
+const[sizes,Setsizes]=useState("");
     const apidata= async()=>  
-    {
-        const newsdata= await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=26dc02fb8f6f45dc993313592a075d1b&page=1");
-        const res= await newsdata.json()
+    { 
+    const newsdata= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page}&PageSize=15`);
+    const res= await newsdata.json()
       Setnewsdata(res.articles);
-    }
+      Setsizes(res.totalResults/10);
+      // console.log(res.totalResults/10)
+      setload(false);
+  }
+  // console.log(sizes);
 useEffect(()=>
 {   
     apidata();
 
  },[]);
+
  const pagePreHandler=async()=>
  {
-    const newsdata= await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=26dc02fb8f6f45dc993313592a075d1b&page=1");
+  setload(true);
+    const newsdata= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page}&PageSize=10`);
     const res= await newsdata.json()
   Setnewsdata(res.articles);
-  Setbtndisable(0);
+  Setpage((page)-1);
+setload(false);
+
  }
  const pageNextHandler=async()=>
- {
-    const newsdata= await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=26dc02fb8f6f45dc993313592a075d1b&page=2");
+ {  
+  setload(true);
+    const newsdata= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page}&PageSize=10`);
     const res= await newsdata.json()
   Setnewsdata(res.articles);
-  Setbtndisable(1);
+  Setpage((page)+1);
+  setload(false);
  }
- 
+//  console.log(page);
 // const news newsdata;
 // console.log(news);
    const newsitem= newsdata.map((element) => {
@@ -41,18 +54,20 @@ useEffect(()=>
              <Newsitem  key={a*Math.random()} imgurl={element.urlToImage?element.urlToImage:"https://cdn.pixabay.com/photo/2017/06/26/19/32/news-2444888_960_720.jpg"}title= {element.title?element.title.slice(0,44):""} description={element.description?element.description.slice(0,65):""}  urlnews={element.url}></Newsitem>
             );
     });
-    
+    // console.log(page,sizes);//
     // console.log(newsitem);
     return (
         <>  
          <h1 id="h1">Top Headlines</h1>
-         <Sipnner/>
+       
+       
           <div className="shownews">
-        {newsitem}
+        {load===true ?<Sipnner/>:newsitem}
         </div>
         <div className="pagebtn">
-        <button type="button" disabled={!btndisable} class="btn btn-danger" id="p1" onClick={pagePreHandler}>Previous</button>
-        <button type="button" disabled={btndisable}class="btn btn-danger" id="n1"onClick={pageNextHandler}>Next</button>
+        <button type="button" disabled={page<=1?true:false} class="btn btn-danger" id="p1" onClick={pagePreHandler}>Previous</button>
+        <button type="button" class="btn btn-danger"disabled={page<sizes?false:true} id="n1"onClick={pageNextHandler}>Next</button>
+   
         </div>
         <br></br>
         <br></br>
