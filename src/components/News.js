49d6b86dff1cf4447f2a7d0a36a0de174dd15041
Anirudh <developeranirudhgupta@gmail.com>
2022-02-03 00:09:35 +0530

    final

--- src/components/News.js
+++ src/components/News.js
@@ -3,7 +3,6 @@ import React,{useEffect} from 'react';
 import { useState } from 'react';
 import Newsitem from './Newsitem';
 // import data from './sampledata.json';
-import 'dotenv/config';
 import './News.css';
 import Sipnner from './Sipnner';
 export default function News(props) {
@@ -13,8 +12,8 @@ const [newsdata,Setnewsdata]=useState([]);
 const[sizes,Setsizes]=useState("");
     const apidata= async()=>  
     { 
-    const newsdata= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${"3bac4428378d4e32af0a4501fc4fb812"}&page=${page}&PageSize=15`);
-    const res= await newsdata.json()
+    const data= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page}&PageSize=15`);
+    const res= await data.json()
       Setnewsdata(res.articles);
       Setsizes(res.totalResults/10);
       // console.log(res.totalResults/10)
@@ -30,7 +29,7 @@ useEffect(()=>
  const pagePreHandler=async()=>
  {
   setload(true);
-    const newsdata= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${"3bac4428378d4e32af0a4501fc4fb812"}&page=${page}&PageSize=10`);
+    const newsdata= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page}&PageSize=10`);
     const res= await newsdata.json()
   Setnewsdata(res.articles);
   Setpage((page)-1);
@@ -39,7 +38,7 @@ setload(false);
  const pageNextHandler=async()=>
  {  
   setload(true);
-    const newsdata= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey="3bac4428378d4e32af0a4501fc4fb812"&page=${page}&PageSize=10`);
+    const newsdata= await fetch(`https://newsapi.org/v2/top-headlines?category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page}&PageSize=10`);
     const res= await newsdata.json()
   Setnewsdata(res.articles);
   Setpage((page)+1);
@@ -52,7 +51,7 @@ setload(false);
          let a=110;        
          return( 
              <Newsitem  key={a*Math.random()} imgurl={element.urlToImage?element.urlToImage:"https://cdn.pixabay.com/photo/2017/06/26/19/32/news-2444888_960_720.jpg"}title= {element.title?element.title.slice(0,44):""} description={element.description?element.description.slice(0,65):""}  urlnews={element.url}></Newsitem>
-          );
+            );
     });
     // console.log(page,sizes);//
     // console.log(newsitem);
@@ -65,8 +64,8 @@ setload(false);
         {load===true ?<Sipnner/>:newsitem}
         </div>
         <div className="pagebtn">
-        <button type="button" disabled={page<=1?true:false} class="btn btn-dark" id="p1" onClick={pagePreHandler}>Previous</button>
-        <button type="button" class="btn btn-dark"disabled={page<sizes?false:true} id="n1"onClick={pageNextHandler}>Next</button>
+        <button type="button" disabled={page<=1?true:false} className="btn btn-dark" id="p1" onClick={pagePreHandler}>Previous</button>
+        <button type="button" className="btn btn-dark"disabled={page<sizes?false:true} id="n1"onClick={pageNextHandler}>Next</button>
    
         </div>
         <br></br>
--- src/components/Sipnner.js
+++ src/components/Sipnner.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Sipnner() {
     return (
         <div className="text-center">
-    <div class="spinner-border text-bg-dark" role="status">
-   <span class="visually-hidden">Loading...</span>
+    <div className="spinner-border text-bg-dark" role="status">
+   <span className="visually-hidden">Loading...</span>
         </div>
         </div>
     )
